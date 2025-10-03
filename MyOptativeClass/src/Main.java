2753cd6fdae8ffd26a0c48ba754068df83bece2e
Danij4ne <danij4ne@gmail.com>
2025-10-03 16:19:53 +0200

    update

---
+++ MyOptativeClass/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //Public = todo el mundo lo puede ver
+    //static = no necesita ningun intermediario para poder ejecutarse , se ejucta directamente
+
+    public static void main(String[] args){  //solo hay un metodo main , siempre es publico , estatic y no retorna nada = avoid y se llama main siempre
+        System.out.println("Este es mi primer programa en Java"); //out = fuera , print = imprimir , ln = salto de linea
+
+    }
+
+    }
+
+//TODO comentario especial que resalta
---
+++ MyProgrammingClass/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    //Public = todo el mundo lo puede ver
+    //static = no necesita ningun intermediario para poder ejecutarse , se ejucta directamente
+
+    public static void main(String[] args){  //solo hay un metodo main , siempre es publico , estatic y no retorna nada = avoid y se llama main siempre
+        System.out.println("Este es mi primer programa en Java"); //out = fuera , print = imprimir , ln = salto de linea
+        System.out.print("Esta es la segunda linea del programa\n\n"); // quito ln y pongo 2 saltos de linea manual
+
+    }
+
+    public void nombreMetodo(){  //metodo / funcion , void = que el metodo no retorna ningun valor,
+
+    }
+
+//TODO comentario especial que resalta
+}
