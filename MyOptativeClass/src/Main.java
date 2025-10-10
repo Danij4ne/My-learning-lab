c94543e8162a3cabd70fcba364e80b5996034258
Danij4ne <danij4ne@gmail.com>
2025-10-10 16:02:15 +0200

    update

--- MyOptativeClass/src/Main.java
+++ MyOptativeClass/src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
-    import new Operaciones()
-        //IMPORT MODEL.OPERACIONES; --> Importar la carpeta operaciones
     //Public = todo el mundo lo puede ver
     //static = no necesita ningun intermediario para poder ejecutarse , se ejucta directamente
 
     public static void main(String[] args){  //solo hay un metodo main , siempre es publico , estatic y no retorna nada = avoid y se llama main siempre
         System.out.println("Este es mi primer programa en Java"); //out = fuera , print = imprimir , ln = salto de linea
         saludar(" Dani");
-        //Operaciones operacionesMatematicas = new operaciones();
+        Operaciones operacionesMatematicas = new Operaciones(); // importamos las funciones de el archivo operaciones
+        //Operaciones = como se llama la carpeta , operacionesMatematicas = nombre variable , new Operaciones() = le das el poder de all lo que hay dentro de la carpeta
+        operacionesMatematicas.sumar(3,2); //utilizamos sus funciones sumar
+        operacionesMatematicas.restar(3,2); //Restar
 
 
 
--- MyOptativeClass/src/Operaciones.java
+++ MyOptativeClass/src/Operaciones.java
@@ -6,11 +6,21 @@ public class Operaciones {
         System.out.println("El resultado de la suma es" +resultado );
     }
 
-    public void restar (int number1 , int number2){ //los puedes declarar ademas static y utilizarlos en Main importando facil pero no es la mejor opcion
-        int resultado = number1 - number2;
-        System.out.println("El resultado de la resta es" +resultado );
+    public void sumar (double number1 , double number2){ //puedes hacer dos metodos con el mismo nombre y parametros siempre que sea diferente el tipo de dato , double , int ...
+        double resultado = number1 + number2;
+        System.out.println("El resultado de la suma es" +resultado );
     }
 
+
+
+    public void restar (int number1 , int number2){ //los puedes declarar ademas static y utilizarlos en Main importando facil pero no es la mejor opcion
+        int resultado = number1 - number2;
+        System.out.println("El resultado de la resta es " +resultado );
+    }
+
+//Aparte de public hay protected y private, protected
+// Protected = solo los documentos que esten en el mismo paquete / carpeta lo podran llamar
+// Private = nadie puede llamarlo menos un metodo de la misma clase
 
 
 
