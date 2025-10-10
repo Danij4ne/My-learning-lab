a8edf0cbabd6a074cff834dafb3e0d9de73350ed
Danij4ne <danij4ne@gmail.com>
2025-10-10 15:50:34 +0200

    update

--- MyOptativeClass/src/Main.java
+++ MyOptativeClass/src/Main.java
@@ -1,11 +1,42 @@
 public class Main {
+    import new Operaciones()
+        //IMPORT MODEL.OPERACIONES; --> Importar la carpeta operaciones
     //Public = todo el mundo lo puede ver
     //static = no necesita ningun intermediario para poder ejecutarse , se ejucta directamente
 
     public static void main(String[] args){  //solo hay un metodo main , siempre es publico , estatic y no retorna nada = avoid y se llama main siempre
         System.out.println("Este es mi primer programa en Java"); //out = fuera , print = imprimir , ln = salto de linea
+        saludar(" Dani");
+        //Operaciones operacionesMatematicas = new operaciones();
+
+
 
     }
+    public static void saludar (String name ){ //crear funcion
+        System.out.println("Hola" +name);
+        System.out.println("Buenos dias");
+        System.out.println("Que tal estas");
+    }
+
+
+//static = que es accesible directamente no necesitas ningun intermediario
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
---
+++ MyOptativeClass/src/Operaciones.java
@@ -0,0 +1,17 @@
+public class Operaciones {
+    //aqui no pones el metodo main porque solo quieres crear funciones para operaciones
+
+    public void sumar (int number1 , int number2){
+        int resultado = number1 + number2;
+        System.out.println("El resultado de la suma es" +resultado );
+    }
+
+    public void restar (int number1 , int number2){ //los puedes declarar ademas static y utilizarlos en Main importando facil pero no es la mejor opcion
+        int resultado = number1 - number2;
+        System.out.println("El resultado de la resta es" +resultado );
+    }
+
+
+
+
+}
