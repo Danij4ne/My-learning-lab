64d2deab8b06d0b1a341122ed9ac426ebbf51464
Daniel Jane <danij4ne@gmail.com>
2025-10-19 15:07:35 +0200

    update

--- ExercicesJava/src/Main.java
+++ ExercicesJava/src/Main.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        //Ex 1
+        Scanner lector = new Scanner(System.in);
+        System.out.print("¿Cuantos bocadillos a comprado?");
+        int numBocadillos = lector.nextInt();
+        System.out.println("¿Que precio tenia cada bocadillo?");
+        double precioBocadillo = lector.nextDouble();
+        System.out.println("¿Cuantas bebidas a comprado?");
+        int numBebidas = lector.nextInt();
+        System.out.println("¿Que precio tenia cada bebida?");
+
+
+
+
+
+
     }
+
+
+
 }
